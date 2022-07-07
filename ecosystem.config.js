120d31ce558380ed2b4e38d755636cbe18d86c5a
mcremer-able <marc.cremer@able-group.de>
2022-07-07 15:28:20 +0200

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,3 +1,4 @@
+
 module.exports = {
   apps : [{
     script: 'app.js',
@@ -11,7 +12,7 @@ module.exports = {
        "ref"  : "origin/master",
        "key" : "~/.ssh/crossinx_user.pem",
        "repo" : "git@github.com:mcremer-able/deploytest",
-       "path" : "./",
+       "path" : "/home/azureuser/www/deploytest",
        "post-setup" : "npm install"
     }
   }
