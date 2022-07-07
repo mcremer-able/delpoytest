3e2fdb00ee8383ae6ce0466b731fe76fba560249
mcremer-able <marc.cremer@able-group.de>
2022-07-07 15:22:09 +0200

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,7 +11,7 @@ module.exports = {
        "ref"  : "origin/master",
        "key" : "~/.ssh/crossinx_user.pem",
        "repo" : "git@github.com:Username/repository.git",
-       "path" : "/var/www/my-repository",
+       "path" : ".",
        "post-setup" : "npm install"
     }
   }
