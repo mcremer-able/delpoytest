04de841a0a920ace21c1fd898ec1d767af6003fc
mcremer-able <marc.cremer@able-group.de>
2022-07-07 15:44:31 +0200

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,19 +1,20 @@
 
 module.exports = {
-  apps : [{
+  apps: [{
     script: 'app.js',
   }],
-   
+
   // Deployment Configuration
-  deploy : {
-    production : {
-       "user" : "azureuser",
-       "host" : ["able-outgoing-dev.westeurope.cloudapp.azure.com"],
-       "ref"  : "origin/main",
-       "key" : "~/.ssh/crossinx_user.pem",
-       "repo" : "https://github.com/mcremer-able/delpoytest.git",
-       "path" : "/home/azureuser/www/deploytest",
-       "post-setup" : "npm install"
-          }
+  deploy: {
+    production: {
+      "user": "azureuser",
+      "host": ["able-outgoing-dev.westeurope.cloudapp.azure.com"],
+      "ref": "origin/main",
+      "key": "~/.ssh/crossinx_user.pem",
+      "repo": "https://github.com/mcremer-able/delpoytest.git",
+      "path": "/home/azureuser/www/deploytest",
+      "pre-deploy": "npm install"
+
+    }
   }
 };
